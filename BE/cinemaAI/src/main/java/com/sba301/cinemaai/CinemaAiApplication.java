deefb5210e08f183fd0426487960330e76e65ded
HieuTuan <tuannhse182788@fpt.edu.vn>
2026-05-16 15:32:06 +0700

    Initialize project

---
+++ BE/cinemaAI/src/main/java/com/sba301/cinemaai/CinemaAiApplication.java
@@ -0,0 +1,13 @@
+package com.sba301.cinemaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaAiApplication.class, args);
+	}
+
+}
