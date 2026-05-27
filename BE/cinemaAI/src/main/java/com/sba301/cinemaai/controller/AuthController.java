250ce672995d12fc52541090496e9afb2383352c
HieuTuan <tuannhse182788@fpt.edu.vn>
2026-05-27 21:49:18 +0700

    fix conflict

--- BE/cinemaAI/src/main/java/com/sba301/cinemaai/controller/AuthController.java
+++ BE/cinemaAI/src/main/java/com/sba301/cinemaai/controller/AuthController.java
@@ -24,11 +24,7 @@ import com.sba301.cinemaai.service.PhoneVerificationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
